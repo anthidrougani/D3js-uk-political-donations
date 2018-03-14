e5c523af35f5c176f076cb4710f62da166db5c47
anthidrougani <p14drou@ionio.gr>
2018-03-14 19:12:39 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -168,7 +168,12 @@ function amountType() {
 		.on("tick", amounts)
 		.start();
 }
+function amounts(e) {
+	node.each(moveToAmount(e.alpha));
 
+		node.attr("cx", function(d) { return d.x; })
+			.attr("cy", function(d) {return d.y; });
+}
 
 function parties(e) {
 	node.each(moveToParties(e.alpha));
@@ -201,7 +206,28 @@ function all(e) {
 		node.attr("cx", function(d) { return d.x; })
 			.attr("cy", function(d) {return d.y; });
 }
-
+function moveToAmount(alpha) {
+	return function(d) {
+		var centreX;
+		var centreY;
+		if (d.value <= 500000){
+			centreX = svgCentre.x +70;
+			centreY = svgCentre.y -70;
+		} else if (d.value <= 5000000){
+			centreX = svgCentre.x +450;
+			centreY = svgCentre.y -70;
+		} else if (d.value <= 10000000){
+			centreX = svgCentre.x +70;
+			centreY = svgCentre.y +250;
+		} else {
+			centreX = svgCentre.x +500;
+			centreY = svgCentre.y +250;
+		}
+		
+		d.x += (centreX - d.x) * (brake + 0.02) * alpha * 1.1;
+		d.y += (centreY - d.y) * (brake + 0.02) * alpha * 1.1;
+	};
+}
 
 function moveToCentre(alpha) {
 	return function(d) {
